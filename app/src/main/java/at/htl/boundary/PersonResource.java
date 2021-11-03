4a42c847110cae543c5cf7bc805fa4202cb7456a
polleichtnermoritz <moritz.polleichtner@outlook.com>
2021-11-03 10:06:40 +0100

    finish project

--- app/src/main/java/at/htl/boundary/PersonResource.java
+++ app/src/main/java/at/htl/boundary/PersonResource.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.time.Instant;
 
-@Path("/api/entry")
+@Path("/api/person")
 @Transactional
 public class PersonResource {
     @Inject
